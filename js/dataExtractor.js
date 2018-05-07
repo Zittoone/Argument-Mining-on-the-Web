878429da6c3fc81bdfa83fd96194f899be54d305
AlexG <alexgreau@live.fr>
2018-05-07 12:06:38 +0200

    init dataExtractor

---
+++ js/dataExtractor.js
@@ -0,0 +1,5 @@
+// TODO
+
+// ouvre fichier annoté
+// scan en sauvergardant dans un tableau les strings  claim %d % jusqu'a tomber sur la premiere logne des relations
+// sauvergarder dans un deuxieme tableau les relations lignes par lignes
