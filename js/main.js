edcc21b1691e283ab30780880224aeadbda57d97
AlexG <alexgreau@live.fr>
2018-05-08 15:04:00 +0200

    Debut Dropdown

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+window.onload = init;
+
+
+function init(){
+    console.log("page chargee");
+
+}
+
+function dropDwnFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("dropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
