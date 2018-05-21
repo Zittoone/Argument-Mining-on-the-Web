b59488d40cd9a6588acf0d4b54437d311c587a9c
AlexGreau <alexgreau@live.fr>
2018-05-21 23:12:04 +0200

    Debut slicing .ann

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-let reader
-let files,elemID,labelID
-
+let reader;
+let files, file, elemID, labelID, lines;
+let indexes, parts;
 window.onload = init;
+
+
+//let regex1 = new RegExp();
 
 
 function init() {
     console.log("page chargee");
+    lines = []
+    parts = []
+    // parts [line] [ID,type,indexDebut, indexFin]
 }
 
 
 function printdoc(type) {
 
-    if (type == 'txt'){
+    if (type == 'txt') {
         elemID = 'txtFiles';
-    }else if (type == 'ann'){
+    } else if (type == 'ann') {
         elemID = 'annFiles';
     }
     labelID = elemID + 'Label';
@@ -24,27 +30,57 @@ function printdoc(type) {
         return;
     }
 
-    var file = files[0];
-    var reader = new FileReader();
-    reader.onloadend = function(evt) {
+    file = files[0];
+    reader = new FileReader();
+    reader.onloadend = function (evt) {
         if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-          document.getElementById('textOutput').textContent = evt.target.result;
+            document.getElementById('textOutput').textContent = evt.target.result;
         }
+        lines = this.result.split('\n');
         document.getElementById(labelID).textContent = file.name
         document.getElementById("titre").textContent = file.name
     }
     reader.readAsText(file);
+    retrieveIndexes();
 }
 
-function annotate(text){
-    // retrieve .ann in var []
-    // analyse 
+function annotate() {
+    // verify .ann is the same title as the mothafucking txt ya
+    // retrieve .ann & isolate infos in var [] to know where to put color
+    // read txt file searching for these arguments mothafucka
+    // write in new file the colored txt
+
+
+    // print that mothafucka
+    // printdoc('rep');
 }
 
-function retrieve (title){
-    // search for title
+function retrieveIndexes() {
+    for (var line = 0; line < lines.length; line++) {
+        // premiere partie de ligne = ID
+        // deuxieme partie = couleur
+        // troisieme partie = range
+        if (lines[line][0] == 'T') {
+            parts[line] = lines[line].split('	', 2);
+            parts[line][1] = parts[line][1].split(' ');
+        } else if (lines[line][0] == 'A') {
+
+        }
+
+        console.log(' end line')
+
+    }
+
+
 }
 
-function colorize(){
+function colorize(type, range) {
     // change color of text
+
+}
+
+function verifyDocs() {
+    // check if titles are the same
+
+    console.log("good mothafuckass");
 }
