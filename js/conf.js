ccac3560ef6eb6e4b61ef0a004c0db67e1a878f4
Thomas <thomasg70360@gmail.com>
2018-05-23 19:25:01 +0200

    titles added, style modified...

--- js/conf.js
+++ js/conf.js
@@ -1,4 +1,2 @@
-//Very important : write here the URL to your project root
-let staticUrl = "/"; // j'ai pas pigé le static url ^^ utilise le realitf plutôt non ? (ça marchait pas chez moi avec localhost)
-
+let staticUrl = "/"; 
 let nodeURL = staticUrl + "datasets/NoDE/";
