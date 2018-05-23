b4a244144f643776ad940e5e021be440c17972a7
Thomas <thomasg70360@gmail.com>
2018-05-23 02:01:48 +0200

    graph lib + drawing it ToDo : Be able to choose every topic on several files

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,6 @@
 let topics = [];
 let nodes = [];
+let graphNodes = [];
 
 let DEBUG = false;
 
@@ -43,15 +44,19 @@ function getTopic(filename) {
         let topic = pairs[i].getAttribute("topic");
         if (!topics.includes(topic)) {
             topics.push(topic);
-            console.log(topic);
+            if (DEBUG) {
+                console.log(topic);
+            }
         }
     }
 }
 
-function getPairs(filename, topic) {
+function constructNodes(filename, topic) {
     let text = readTextFile(filename);
 
     let parser, xmlDoc;
+
+    let test = 0;
 
     parser = new DOMParser();
     xmlDoc = parser.parseFromString(text,"text/xml");    
@@ -62,7 +67,7 @@ function getPairs(filename, topic) {
     for (i = 0; i < pairs.length; i++) { 
         let readTopic = pairs[i].getAttribute("topic");
         if (readTopic == topic) {
-
+            test++;
             let id = pairs[i].childNodes[1].getAttribute("id");
             let text = pairs[i].childNodes[1].childNodes[0].nodeValue;
             let fatherId = pairs[i].childNodes[3].getAttribute("id");
@@ -74,8 +79,60 @@ function getPairs(filename, topic) {
                                 ));
                 hypothesisFound = true;
             }
+
+            nodes.push(new Node(id, text, fatherId, entailment));
         }
     }
+}
+
+function drawGraph() {
+    let graph = new Springy.Graph();
+
+    //Adding nodes
+    for(i = 0; i < nodes.length; i++) {
+        let node = graph.newNode({
+            label: nodes[i].id,
+            ondoubleclick: function() {
+                console.log(nodes[i].text);
+            }
+        });
+
+        graphNodes.push(node);
+    }
+
+    //Adding edges
+    for(let x = 1; x < nodes.length; x++) {
+        let currentNode, fatherNode, color;
+
+        currentNode = graphNodes[x];
+        fatherNode = graphNodes[getNodeGraphIndex(nodes[x].fatherId)];
+
+
+        if (nodes[x].entailment == "YES") {
+            edgeColor = '#7DBE3C'
+        } else {
+            edgeColor = '#FF0000';
+        }
+
+        graph.newEdge(currentNode, fatherNode, {color: edgeColor});
+    }
+
+
+    jQuery(function(){
+        var springy = jQuery('#canvasSpringy').springy({
+          graph: graph
+        });
+      });
+}
+
+function getNodeGraphIndex(id) {
+    let index = -1;
+    for(i = 0; i < nodes.length; i++) {
+        if (nodes[i].id == id) {
+            index = i;
+        }
+    }
+    return index;
 }
 
 class Node {
@@ -84,8 +141,10 @@ class Node {
         this.text = text;
         this.fatherId = fatherId;
         this.entailment = entailment;
+
     }
 }
 
-getTopic(datasetsNoDEUrl + "debatepedia_test.xml");
-getPairs(datasetsNoDEUrl + "debatepedia_test.xml", topics[0]);
+getTopic("http://localhost/datasets/NoDE/debatepedia_test.xml");
+constructNodes("http://localhost/datasets/NoDE/debatepedia_test.xml", "Cellphones");
+drawGraph();
